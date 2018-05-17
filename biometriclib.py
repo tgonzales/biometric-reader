e8799ee9755a85c8e29f03cb86d7dbbef7d4f851
Cleiton Couto <ccouto@mwneo.com>
2018-05-16 23:10:38 -0300

    simplificando a lib, removendo payload

--- biometriclib.py
+++ biometriclib.py
@@ -14,17 +14,17 @@ cmd
 O programa tem 2 threads
 
 a thread principal, executa os seguintes metodos
-init() iniciar a comunicaÃ§Ã£o com o driver
-close() fechar a comunicaÃ§Ã£o com o driver
+init() iniciar a comunicao com o driver
+close() fechar a comunicao com o driver
 run() manda comandos para o driver que sao armazenados na variavel global CMD
 stream() pega a ultima mensagem do driver.
 
-Em paralelo Ã© executado a segunda thread
+Em paralelo eh executado a segunda thread
 run_thread_process()
 esse metodo fica checando a variavel CMD
 para ele o importante sao os CMD 3,4 e 5
-ify - identification (1 para 1 - compara a mao que esta no sensor se eh valida com a que esta em um array unico)
-vfy - verification (1 para N - compara a mao que esta no sensor se eh valida com as maos que estao em um array multiplo)
+ify - identification (1 para 1 - compara a mao que esta no sensor se for valida com a que esta em um array unico)
+vfy - verification (1 para N - compara a mao que esta no sensor se for valida com as maos que estao em um array multiplo)
 enr - grava o template da mao
 
 A necessidade de ter 2 threads eh pela necessidade de termos que interromper um processo e iniciar outro.
@@ -36,7 +36,6 @@ run(3) inicia ify
 
 isso acontece porque ify, vfy, enr rodam em um while entao precisamos dar um break para entrar em outro CMD
 
-dentro de __main__ esta um teste que ao executar o arquivo diretamente roda um fluxo basico de operacao
 '''
 import threading
 import time
@@ -47,7 +46,6 @@ CMD = 0
 STREAM = 'STOPED'
 TMPL = None
 TMPLS = []
-PAYLOAD = False
 
 def run_thread_process():
     global CMD
@@ -89,13 +87,12 @@ def removeDatabase():
 def close():
     t._stop()
 
-def run(cmd, data=False, payload=False):
-    global CMD
+def run(cmd, data=False):
+    global CMD, TMPL, TMPLS
     if cmd == 3:
         TMPL = data
     if cmd == 4:
         TMPLS = data
-    PAYLOAD = payload
     CMD = cmd
 
 def stream():
@@ -108,40 +105,32 @@ def ify():
     c.execute('SELECT * FROM users')
     tmp_list = c.fetchall()
 
-    global CMD, STREAM, PAYLOAD
+    global CMD, STREAM
     while True:
         time.sleep(0.2)
         if CMD == 2:
-            for template in tmp_list:
-                print(template[1])
-                if 'X' == template[1]:
-                    print('user Encontrado')
-                    break
+            break
 
-        STREAM = 'IFY: Coloque a mao no sensor'
-        # compare TMPL
-
-    if PAYLOAD: #Apos checagem fazer transacao de sucesso/erro se necessario
-        with urllib.request.urlopen('http://www.python.org/') as f:
-            print('FETCH URL', f.read(100).decode('utf-8'))
+        for template in tmp_list:
+            STREAM = 'IFY: Coloque a mao no sensor'
+            # compare TMPL
+            # print(template[1])
 
 def vfy():
-    global CMD, TMPLS, STREAM, PAYLOAD
+    # 1:1
+    global CMD, TMPL, STREAM
     while True:
         time.sleep(0.2)
         if CMD == 2:
-            if PAYLOAD:
-                with urllib.request.urlopen('http://www.python.org/') as f:
-                    print('FETCH URL', f.read(100).decode('utf-8'))
             break
-    
+
+        # compara TMPL com biometric
         STREAM = 'VFY: Coloque a mao no sensor'
-        # compare palm in TMPLS
 
 def enr():
     global CMD, STREAM
     while True:
-        time.sleep(0.2)
+        #time.sleep(0.2)
         if CMD == 2:
             break
         STREAM = 'ENR: Coloque a mao no sensor'
--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 from biometriclib import *
+# methods run(),  init(), stream(), createDatabase()
 
 createDatabase()
 _time = 0.3
@@ -6,15 +7,19 @@ init()
 time.sleep(_time)
 print(stream())
 time.sleep(_time)
+
+# ify - 1:1
 run(3, 'MyTemplate')
 for i in range(5):
     time.sleep(_time)
     print(stream())
 
+# break
 run(2)
 print('Run 2 - break proccess 3 and start 5')
 time.sleep(3)
 
+# enr grava template
 run(5)
 print('Run 5')
 time.sleep(_time + 1)
@@ -22,15 +27,19 @@ for i in range(5):
     time.sleep(_time)
     print(stream())
 
+# break
 run(2)
 print('Run 2 - break proccess 5 and start 4')
 time.sleep(3)
 
-run(4, [1,2,3])
+# vfy 1:N
+run(4)
 for i in range(5):
     time.sleep(_time)
     print(stream())
 
 removeDatabase()
+
+# Close
 run(0)
 print('FINISH ...')
